03b894e582550c8b78ab242baa5f619bf678d696
Antony <anto.waweru2030@gmail.com>
2025-09-28 15:00:45 +0300

    set up done

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,13 +1,20 @@
 import Link from "next/link";
+import React from "react";
 
-const PropertyCard: React.FC<PropertyCardProps> = ({ property }) => {
+const Header = () => {
   return (
-    <div className="bg-white rounded-xl overflow-hidden shadow-sm hover:shadow-md transition-shadow duration-300 cursor-pointer">
-      {/* Property Image */}
-      <div className="relative h-64 w-full">
-        <Image
-          src={property.image}
-          alt={property.name}
-          fill
-          style={{ objectFit: 'cover' }} 
-          sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
+    <header className="bg-gray-900 text-white p-4 flex justify-center gap-8">
+      <Link href="/home" className="hover:underline">
+        Home
+      </Link>
+      <Link href="/about" className="hover:underline">
+        About
+      </Link>
+      <Link href="/posts" className="hover:underline">
+        Posts
+      </Link>
+    </header>
+  );
+};
+
+export default Header;
